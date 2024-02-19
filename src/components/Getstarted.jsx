d1ec84b85a772b56dff87e0ec12e97b5f1852848
alffy007 <jamesbond006aj@gmail.com>
2024-02-19 12:14:39 +0530

    Revert "response"
    This reverts commit 52b26bd854426fc1752336fc1f97b52cb21b9bec.

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -3,8 +3,8 @@ import { WhatsApp } from '@mui/icons-material'
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 const Getstarted = () => {
   return (
-    <div className='flex sm:w-44 w-36 cursor-pointer  ease-in duration-300 rounded-3xl hover:scale-105 hover:w-48 hover:bg-green-500 justify-between bg-black text-white sm:px-4 sm:py-3 px-2 py-2'>
-        <WhatsApp fontSize='medium' />{" "}
+    <div className='flex w-44 cursor-pointer  ease-in duration-300 rounded-3xl hover:scale-105 hover:w-48 hover:bg-green-500 justify-between bg-black text-white px-4 py-3'>
+        <WhatsApp/>{" "}
         Getstarted{" "}
         <ArrowOutwardIcon/>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo1 from '../assets/logo.png'
+import logo1 from '../assets/logo1.png'
 import { Button } from '@mui/material'
 
 const Navbar = () => {
   return (
-    <nav className=' font-poppins w-full flex py-6 px-3 sm:p-4 justify-between items-center '>
-    <img src={logo1} className="w-32" alt="logo" />
-    <div className=' cursor-pointer bg-gray-300  hover:bg-green-500 ease-in duration-300 px-3 py-2 rounded-3xl hover:text-white text-gray-00 font-semibold'>
+    <nav className=' font-poppins w-full flex py-6 justify-between items-center '>
+    <img src={logo1} width={124} height={32} alt="logo" />
+    <div className=' cursor-pointer bg-gray-300  hover:bg-green-500 ease-in duration-300 px-4 py-2 rounded-3xl hover:text-white text-gray-00 font-semibold'>
         Login
     </div>
     </nav>

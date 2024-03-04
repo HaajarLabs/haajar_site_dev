64b59b4624bcfba19554475db750091954121ad8
alffy007 <jamesbond006aj@gmail.com>
2024-03-04 11:18:41 +0530

    otp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import logo1 from '../assets/logofirst.png'
+import logo1 from '../assets/logofirst1.png'
 import { Link } from "react-router-dom";
-import logo2 from '../assets/logosecond.png'
+import logo2 from '../assets/logosecond1.png'
+import logoicon from '../assets/logoicon.png'
 
 const Navbar = () => {
 
@@ -10,7 +11,7 @@ const Navbar = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setIsFlipped(prevState => !prevState);
-    }, 2000);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
@@ -18,7 +19,7 @@ const Navbar = () => {
 
   return (
     <nav className=' font-poppins w-full flex py-6 justify-between items-center '>
-    <img   src={isFlipped ? logo1 : logo2} className="w-44 ease-linear transition-opacity duration-1000" alt="logo" />
+    <div className='flex items-center '><img src={logoicon} className="w-9 animate-flipbottom transition-opacity duration-1000" alt="logo" /><img   src={isFlipped ? logo1 : logo2} className="w-28 ease-linear transition-opacity duration-1000" alt="logo" /></div>
     <div className=' cursor-pointer bg-gray-300  hover:bg-[#25D366] ease-in duration-300 px-4 py-2 rounded-3xl hover:text-white text-gray-00 font-semibold'>
        <Link to='/Login'>Login</Link>
     </div>
---
+++ src/widgets/Otpfield.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+
+const OtpField = () => {
+  const inputRefs = useRef([]);
+
+  const focusNextInput = index => {
+    if (inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const focusPrevInput = index => {
+    if (inputRefs.current[index - 1]) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handleChange = (e, index) => {
+    const { value } = e.target;
+    if (value.length === 1) {
+      focusNextInput(index);
+    } else if (value.length === 0) {
+      focusPrevInput(index);
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === 'Backspace' && e.target.value === '') {
+      focusPrevInput(index);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center ">
+      {[...Array(4)].map((_, index) => (
+        <input
+          key={index}
+          ref={el => (inputRefs.current[index] = el)}
+          type="text"
+          maxLength={1}
+          className="w-14 h-14 text-center mx-2 rounded border border-gray-400 focus:outline-none"
+          onChange={e => handleChange(e, index)}
+          onKeyDown={e => handleKeyDown(e, index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default OtpField;

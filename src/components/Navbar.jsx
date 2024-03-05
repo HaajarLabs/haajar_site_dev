d1cb9d66b4f6cd477cc6fe0e1085801cde45552a
alffy007 <jamesbond006aj@gmail.com>
2024-03-05 21:38:21 +0530

    navbar updATRES

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,8 @@ const Navbar = () => {
 
   return (
     <nav className=" font-poppins w-full flex py-6 justify-between items-center ">
-      <div className="flex items-center gap-2">
+       <Link to='/'>
+    <div className="flex items-center gap-2 ">
         <img
           src={logoicon}
           className="w-9 animate-flipbottom transition-opacity duration-1000"
@@ -29,6 +30,7 @@ const Navbar = () => {
           alt="logo"
         />
       </div>
+    </Link>
       <div className=" cursor-pointer bg-gray-300 z-50  hover:bg-[#25D366] ease-in duration-300 px-4 py-2 rounded-3xl hover:text-white text-gray-00 font-semibold">
         <Link to="/Login">Login</Link>
       </div>

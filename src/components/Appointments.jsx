0e4a1acc247515a972e5353445c4fce5bcd1253c
alffy007 <jamesbond006aj@gmail.com>
2024-06-30 22:42:05 +0530

    updates dates

--- src/components/Appointments.jsx
+++ src/components/Appointments.jsx
@@ -14,23 +14,26 @@ function Appointments() {
   const handleSelect = (option) => {
     setSelectedOption(option);
   };
+  const formatNumber = (number) => {
+    return number.toString().padStart(2, '0');
+  };
 
   const date = new Date();
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  const today = year + "-" + "0" + month + "-" + day;
+  const today = year + "-" + "0" + month + "-" + formatNumber(day);
   const [selectedOption, setSelectedOption] = useState(today);
   date.setDate(date.getDate() + 1);
   const daytom = date.getDate();
   const monthtom = date.getMonth() + 1;
   const yeartom = date.getFullYear();
-  const tomorrow = yeartom + "-" + "0" + monthtom + "-" + daytom;
+  const tomorrow = yeartom + "-" + "0" + monthtom + "-" + formatNumber(daytom);
   date.setDate(date.getDate() + 1);
   const daybf = date.getDate();
   const monthbf = date.getMonth() + 1;
   const yearbf = date.getFullYear();
-  const dayAfterTomorrow = yearbf + "-" + "0" + monthbf + "-" + daybf;
+  const dayAfterTomorrow = yearbf + "-" + "0" + monthbf + "-" + formatNumber(daybf);
   const options = [today, tomorrow, dayAfterTomorrow];
   const [doc_name, setDocName] = useState("Dr. Haajar");
   // Fetch appointments data here

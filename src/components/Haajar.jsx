79fe6c82140d55efa580d313908ad72077da366b
alffy007 <jamesbond006aj@gmail.com>
2024-07-08 20:05:36 +0530

    mobil res1

--- src/components/Haajar.jsx
+++ src/components/Haajar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import queue from "../assets/queue.svg";
 const Haajar = () => {
   return (
-    <div className="relative h-screen flex  mx-auto bg-[#F43F5E] pt-6 pl-28 pb-6 justify-between overflow-hidden ">
+    <div className="relative lg:h-screen h-fit flex  mx-auto bg-[#F43F5E] lg:pt-6 py-16 px-3 lg:pl-28 lg:pb-6 justify-between overflow-hidden ">
       <div
         data-aos="fade-down"
         data-aos-once="true"
@@ -17,21 +17,21 @@ const Haajar = () => {
         data-aos-duration="1000"
         className="absolute bottom-0 left-0 right-0 h-52 bg-gradient-to-t from-black/50 to-transparent pointer-events-none "
       ></div>
-      <div className="mt-40 w-3/6 flex flex-col items-center">
+      <div className="lg:mt-40 lg:w-3/6 flex flex-col items-center">
         <h1
           data-aos="fade-right"
           data-aos-once="true"
           data-aos-duration="1000"
           className="text-5xl font-bold mb-6 font-vilane_Regular text-white"
         >
-          Why Haajar?
+          Why Haajar ?
         </h1>
         <p
           data-aos="flip-left"
           data-aos-once="true"
           data-aos-delay="400"
           data-aos-duration="1000"
-          className=" bg-white text-center text-2xl leading-loose tracking-wider text-[#6E6E6E] rounded-lg   font-vilane_Medium  shadow-2xl p-12"
+          className=" bg-white text-center lg:text-2xl text-xl leading-loose tracking-wider text-[#6E6E6E] rounded-lg   font-vilane_Medium  shadow-2xl p-8 lg:p-12"
         >
           Struggling with long wait times and managing large crowds? Imagine
           effortlessly optimizing appointments and ensuring a smoother customer
@@ -47,7 +47,7 @@ const Haajar = () => {
         data-aos-duration="1000"
         src={queue}
         alt="image"
-        className="scale-120"
+        className="scale-120 hidden lg:block"
       />
     </div>
   );
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import mockup from '../assets/Group 13.svg';
+import React from "react";
+import mockup from "../assets/Group 13.svg";
 const Features = () => {
-    return (
-        <div className='px-10    py-16 flex flex-col  items-center w-full ' >
-            <h1 data-aos="fade-up"   data-aos-once="true"       data-aos-duration="1000" className='pt-3 text-4xl  font-vilane_Semi_bold ' >Introducing</h1>
-            <p data-aos="fade-up"    data-aos-once="true"        data-aos-delay="400"  data-aos-duration="1000" className='text-[400px] tracking-widest font-vilane_bold -mt-36 text-[#F43F5E] drop-shadow-neumorphism' >Haajar</p>
-            <div className='h-60'></div>
-       <img  data-aos="fade-up"   data-aos-once="true"     data-aos-delay="800"  data-aos-duration="1000" className=' absolute  mt-60   w-4/6' src={mockup} alt="mockup" />
-        </div>
-    );
+  return (
+    <div className="px-10    lg:py-16 flex flex-col  items-center w-full ">
+      <h1
+        data-aos="fade-up"
+        data-aos-once="true"
+        data-aos-duration="1000"
+        className="pt-3 text-4xl  font-vilane_Semi_bold "
+      >
+        Introducing
+      </h1>
+      <p
+        data-aos="fade-up"
+        data-aos-once="true"
+        data-aos-delay="400"
+        data-aos-duration="1000"
+        className="lg:text-[400px] text-8xl tracking-widest font-vilane_bold lg:-mt-36 text-[#F43F5E] drop-shadow-neumorphism"
+      >
+        Haajar
+      </p>
+      <div className="lg:h-60 h-32"></div>
+      <img
+        data-aos="fade-up"
+        data-aos-once="true"
+        data-aos-delay="800"
+        data-aos-duration="1000"
+        className=" absolute  lg:mt-60 mt-28   w-4/6"
+        src={mockup}
+        alt="mockup"
+      />
+    </div>
+  );
 };
 
 export default Features;
